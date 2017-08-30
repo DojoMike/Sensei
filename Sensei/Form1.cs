392694943560d0e8a331b55151e09d26646791bc
DojoMike <mfox32322@gmail.com>
2017-08-29 21:45:40 -0400

    Set up text-to-speech using SAPI * Created a Speaker class that's basically just an abstraction layer * Created a form and set up the UI to change which voice is used
    NEXT:
    * Get it to save the voice to a file, so users don't have to do this every time
    * Figure out the file format to use (JSON, CSV, XML, etc.) for the workout files and the preferences file (guess that should've come first lol)
    * Find out if there are other SAPI settings users should be able to control, and build them into the form if needed

---
+++ Sensei/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sensei
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // All WinForms apps need this
+            InitializeComponent();
+
+            // Set up the dropdown menu with the list of voices
+            var voices = Speaker.GetVoices();
+            if (voices.Length > 0)
+            {
+                foreach (string voice in voices)
+                    Voice.Items.Add(voice);
+                Voice.Text = voices[0];
+            }
+            else
+                MessageBox.Show("Unable to find installed voices.");
+        }
+
+        private void SetVoice(object sender, EventArgs e)
+        {
+            Speaker.SetVoice(Voice.Text);
+            Speaker.Say(Voice.Text);
+        }
+    }
+}
---
+++ Sensei/Speaker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using SpeechLib;
+
+namespace Sensei
+{
+    class Speaker
+    {
+        private static SpVoice sapi = new SpVoice();
+        
+        /// <summary>
+        /// Uses Microsoft Speech API ("SAPI") to speak a string of text
+        /// </summary>
+        /// <param name="text">The text to speak</param>
+        public static void Say(string text)
+        {
+            sapi.Speak(text);
+        }
+
+        /// <summary>
+        /// Gets the list of installed voices
+        /// </summary>
+        /// <returns>The list of voices</returns>
+        public static string[] GetVoices()
+        {
+            var data = new List<string>();
+            var voices = sapi.GetVoices();
+            for (int i=0; i<voices.Count; i++)
+                data.Add(voices.Item(i).GetDescription());
+            return data.ToArray();
+        }
+
+        /// <summary>
+        /// Sets the voice to use when speaking
+        /// </summary>
+        /// <param name="voice">The "name" of the voice (Zira, David etc.)</param>
+        public static void SetVoice(string voice)
+        {
+            var voices = sapi.GetVoices();
+            for (int i = 0; i < voices.Count; i++)
+                if (voices.Item(i).GetDescription().Equals(voice))
+                {
+                    sapi.Voice = voices.Item(i);
+                    return;
+                }
+        }
+    }
+}
