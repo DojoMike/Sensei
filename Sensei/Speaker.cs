33863c6f25153b994a64e226404f7e54782950d3
DojoMike <mfox32322@gmail.com>
2017-08-30 05:00:31 -0400

    Added rate and volume to Speaker class

--- Sensei/Speaker.cs
+++ Sensei/Speaker.cs
@@ -30,6 +30,36 @@ namespace Sensei
         }
 
         /// <summary>
+        /// Volume (0 to 100)
+        /// </summary>
+        public static int Volume
+        {
+            get
+            {
+                return sapi.Volume;
+            }
+            set
+            {
+                sapi.Volume = value;
+            }
+        }
+
+        /// <summary>
+        /// Rate (-10 to 10, 10 being the fastest)
+        /// </summary>
+        public static int Rate
+        {
+            get
+            {
+                return sapi.Rate;
+            }
+            set
+            {
+                sapi.Rate = value;
+            }
+        }
+
+        /// <summary>
         /// Sets the voice to use when speaking
         /// </summary>
         /// <param name="voice">The "name" of the voice (Zira, David etc.)</param>
